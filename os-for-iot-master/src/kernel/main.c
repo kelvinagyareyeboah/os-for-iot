9ef990cff0839c23a74dc18ec72086ee49d6a634
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-30 20:39:51 +0000

    Update main.c

--- os-for-iot-master/src/kernel/main.c
+++ os-for-iot-master/src/kernel/main.c
@@ -1,5 +1,4 @@
-
-            // Buffer and string arr
+arr
             char temp_str[12], hum_str[12];
                  itoa(simulated_temp, temp_str);
             itoa(simulated_humidity,             int idx = 0; // Index for buf
