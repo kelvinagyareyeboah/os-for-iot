4aa5116acd9fe2b89c6a72110569a56f63c2b5da
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-30 20:58:13 +0000

    Update main.c

--- os-for-iot-master/src/kernel/main.c
+++ os-for-iot-master/src/kernel/main.c
@@ -1,10 +1,5 @@
 
-            unsigned long start 
-        schedule(); // Run scheduler to switch tasks
-        cycle++; // Increment cycle counter
-        
-        // Periodically display system statistics
-        if (cycle % 50 == 0) { // Every ~50 schedules
+            unsignedschedules
             uart_puts("   [SYS] Tasks running, CPU idle ~80%, Power: Low\n");
         }
     }
