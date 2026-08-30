39cfef83cd8e40c0bbb279be76d28e4b168a932e
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-30 20:54:10 +0000

    Update main.c

--- os-for-iot-master/src/kernel/main.c
+++ os-for-iot-master/src/kernel/main.c
@@ -1,7 +1,6 @@
 
 {
-    {
-        uart_puts("   [MQTT] Establishing secure connection (TLS handshake)...\n");
+    {cure connection (TLS handshake)...\n");
         for (volatile int i = 0; i < 5000000; i++); // Simulate network delay
 
         uart_puts("   [MQTT] Publishing sensor data to broker...\n");
