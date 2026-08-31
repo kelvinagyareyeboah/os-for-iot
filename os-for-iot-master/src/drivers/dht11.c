ec8f1317b46c649220f345299fd447feb96bb892
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-31 01:18:24 +0000

    Update dht11.c

--- os-for-iot-master/src/drivers/dht11.c
+++ os-for-iot-master/src/drivers/dht11.c
@@ -1,4 +1,4 @@
 
-        // If > 50us (usually 70us for 
+        // If > 50us (u
                 else if (i < 16                rere |= (1 << (23 -     res.succe    gpio_init_output(DHT_PIN);
     g
