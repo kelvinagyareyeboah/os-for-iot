00a043a48fd0767fa97f9b767c21d0c7a01f7ef8
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-31 01:16:41 +0000

    Update dht11.c

--- os-for-iot-master/src/drivers/dht11.c
+++ os-for-iot-master/src/drivers/dht11.c
@@ -2,6 +2,4 @@
         // If > 50us (usually 70us for '1', 26-28us f
                     /
                 else if (i < 16                res.temperature |= (1 << (23 -     res.succe    gpio_init_output(DHT_PIN);
-    gpio
-    return res;
-}
+    g
