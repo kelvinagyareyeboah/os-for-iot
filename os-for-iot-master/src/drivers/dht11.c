6d574d31e773b9a19d7d66ab2c02b998c5bc6e6e
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-08-31 01:16:21 +0000

    Update dht11.c

--- os-for-iot-master/src/drivers/dht11.c
+++ os-for-iot-master/src/drivers/dht11.c
@@ -1,7 +1,7 @@
 
 
         // If > 50us (usually 70us for '1', 26-28us for '0'), it's a 1
-                    // Shift info
+                    /
                 else if (i < 16                res.temperature |= (1 << (23 -     res.success = 1;
 
     // Reset pin to high (Idle)
